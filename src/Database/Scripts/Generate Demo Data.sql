83b5ba92f3481d14a0c9b17b04cbca9141825b07
DMEvans <dmevans1201@gmail.com>
2017-03-23 14:48:59 +0000

    Updated demo data generation script

--- src/Database/Scripts/Generate Demo Data.sql
+++ src/Database/Scripts/Generate Demo Data.sql
@@ -1,4 +1,4 @@
-SET IDENTITY_INSERT [dbo].[Genders] ON 
+﻿SET IDENTITY_INSERT [dbo].[Genders] ON 
 
 GO
 INSERT [dbo].[Genders] ([Id], [Name]) VALUES (1, N'Male')
@@ -12,19 +12,19 @@ GO
 SET IDENTITY_INSERT [dbo].[Customers] ON 
 
 GO
-INSERT [dbo].[Customers] ([Id], [Title], [FirstName], [LastName], [DateOfBirth], [GenderId]) VALUES (1, N'Mr', N'Quinn', N'Dyer', CAST(N'1962-05-05 00:00:00.000' AS DateTime), 1)
+INSERT [dbo].[Customers] ([Id], [Title], [FirstName], [LastName], [DateOfBirth], [GenderId]) VALUES (1, N'Mr', N'Quinn', N'Dyer', CAST(N'1962-May-05 00:00:00' AS DateTime), 1)
 GO
-INSERT [dbo].[Customers] ([Id], [Title], [FirstName], [LastName], [DateOfBirth], [GenderId]) VALUES (3, N'Mr', N'Phillip', N'Firmin', CAST(N'1981-09-22 00:00:00.000' AS DateTime), 1)
+INSERT [dbo].[Customers] ([Id], [Title], [FirstName], [LastName], [DateOfBirth], [GenderId]) VALUES (3, N'Mr', N'Phillip', N'Firmin', CAST(N'1981-Sep-22 00:00:00' AS DateTime), 1)
 GO
-INSERT [dbo].[Customers] ([Id], [Title], [FirstName], [LastName], [DateOfBirth], [GenderId]) VALUES (4, N'Mrs', N'Claire', N'Fishman', CAST(N'1992-11-19 00:00:00.000' AS DateTime), 2)
+INSERT [dbo].[Customers] ([Id], [Title], [FirstName], [LastName], [DateOfBirth], [GenderId]) VALUES (4, N'Mrs', N'Claire', N'Fishman', CAST(N'1992-Nov-19 00:00:00' AS DateTime), 2)
 GO
-INSERT [dbo].[Customers] ([Id], [Title], [FirstName], [LastName], [DateOfBirth], [GenderId]) VALUES (6, N'Miss', N'Miriam', N'Ridley', CAST(N'1985-05-03 00:00:00.000' AS DateTime), 2)
+INSERT [dbo].[Customers] ([Id], [Title], [FirstName], [LastName], [DateOfBirth], [GenderId]) VALUES (6, N'Miss', N'Miriam', N'Ridley', CAST(N'1985-May-03 00:00:00' AS DateTime), 2)
 GO
-INSERT [dbo].[Customers] ([Id], [Title], [FirstName], [LastName], [DateOfBirth], [GenderId]) VALUES (7, N'Mr', N'Thaddeus', N'Brownlow', CAST(N'1955-07-06 00:00:00.000' AS DateTime), 1)
+INSERT [dbo].[Customers] ([Id], [Title], [FirstName], [LastName], [DateOfBirth], [GenderId]) VALUES (7, N'Mr', N'Thaddeus', N'Brownlow', CAST(N'1955-Jul-06 00:00:00' AS DateTime), 1)
 GO
-INSERT [dbo].[Customers] ([Id], [Title], [FirstName], [LastName], [DateOfBirth], [GenderId]) VALUES (8, N'Miss', N'Lorna', N'Nash', CAST(N'1996-01-01 00:00:00.000' AS DateTime), 2)
+INSERT [dbo].[Customers] ([Id], [Title], [FirstName], [LastName], [DateOfBirth], [GenderId]) VALUES (8, N'Miss', N'Lorna', N'Nash', CAST(N'1996-Jan-01 00:00:00' AS DateTime), 2)
 GO
-INSERT [dbo].[Customers] ([Id], [Title], [FirstName], [LastName], [DateOfBirth], [GenderId]) VALUES (10, N'Mr', N'Arnold', N'Johnson', CAST(N'1991-01-01 00:00:00.000' AS DateTime), 1)
+INSERT [dbo].[Customers] ([Id], [Title], [FirstName], [LastName], [DateOfBirth], [GenderId]) VALUES (10, N'Mr', N'Arnold', N'Johnson', CAST(N'1991-Jan-01 00:00:00' AS DateTime), 1)
 GO
 SET IDENTITY_INSERT [dbo].[Customers] OFF
 GO
@@ -52,17 +52,17 @@ GO
 SET IDENTITY_INSERT [dbo].[Orders] ON 
 
 GO
-INSERT [dbo].[Orders] ([Id], [CustomerId], [DeliveryAddressId], [InvoiceAddressId], [CreatedTimestamp], [IsSent]) VALUES (4, 1, 3, 3, CAST(N'2017-02-25 13:14:00.000' AS DateTime), 0)
+INSERT [dbo].[Orders] ([Id], [CustomerId], [DeliveryAddressId], [InvoiceAddressId], [CreatedTimestamp], [IsSent]) VALUES (4, 1, 3, 3, CAST(N'2017-Feb-25 13:14:00' AS DateTime), 0)
 GO
-INSERT [dbo].[Orders] ([Id], [CustomerId], [DeliveryAddressId], [InvoiceAddressId], [CreatedTimestamp], [IsSent]) VALUES (9, 1, 7, 3, CAST(N'2017-02-25 13:25:00.000' AS DateTime), 1)
+INSERT [dbo].[Orders] ([Id], [CustomerId], [DeliveryAddressId], [InvoiceAddressId], [CreatedTimestamp], [IsSent]) VALUES (9, 1, 7, 3, CAST(N'2017-Feb-25 13:25:00' AS DateTime), 1)
 GO
-INSERT [dbo].[Orders] ([Id], [CustomerId], [DeliveryAddressId], [InvoiceAddressId], [CreatedTimestamp], [IsSent]) VALUES (11, 3, 10, 10, CAST(N'2017-02-25 13:29:00.000' AS DateTime), 0)
+INSERT [dbo].[Orders] ([Id], [CustomerId], [DeliveryAddressId], [InvoiceAddressId], [CreatedTimestamp], [IsSent]) VALUES (11, 3, 10, 10, CAST(N'2017-Feb-25 13:29:00' AS DateTime), 0)
 GO
-INSERT [dbo].[Orders] ([Id], [CustomerId], [DeliveryAddressId], [InvoiceAddressId], [CreatedTimestamp], [IsSent]) VALUES (12, 6, 12, 12, CAST(N'2017-02-25 13:31:00.000' AS DateTime), 1)
+INSERT [dbo].[Orders] ([Id], [CustomerId], [DeliveryAddressId], [InvoiceAddressId], [CreatedTimestamp], [IsSent]) VALUES (12, 6, 12, 12, CAST(N'2017-Feb-25 13:31:00' AS DateTime), 1)
 GO
-INSERT [dbo].[Orders] ([Id], [CustomerId], [DeliveryAddressId], [InvoiceAddressId], [CreatedTimestamp], [IsSent]) VALUES (13, 7, 16, 15, CAST(N'2017-02-25 13:34:00.000' AS DateTime), 0)
+INSERT [dbo].[Orders] ([Id], [CustomerId], [DeliveryAddressId], [InvoiceAddressId], [CreatedTimestamp], [IsSent]) VALUES (13, 7, 16, 15, CAST(N'2017-Feb-25 13:34:00' AS DateTime), 0)
 GO
-INSERT [dbo].[Orders] ([Id], [CustomerId], [DeliveryAddressId], [InvoiceAddressId], [CreatedTimestamp], [IsSent]) VALUES (14, 8, 19, 19, CAST(N'2017-02-25 13:35:00.000' AS DateTime), 0)
+INSERT [dbo].[Orders] ([Id], [CustomerId], [DeliveryAddressId], [InvoiceAddressId], [CreatedTimestamp], [IsSent]) VALUES (14, 8, 19, 19, CAST(N'2017-Feb-25 13:35:00' AS DateTime), 0)
 GO
 SET IDENTITY_INSERT [dbo].[Orders] OFF
 GO
@@ -78,41 +78,41 @@ GO
 SET IDENTITY_INSERT [dbo].[Products] ON 
 
 GO
-INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (2, 2, N'Rogue One: A Star Wars Story', 14.5000, N'images/products/rogueone.jpg')
+INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (2, 2, N'Rogue One: A Star Wars Story', 14.50, N'images/products/rogueone.jpg')
 GO
-INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (3, 2, N'Doctor Strange', 13.0000, N'images/products/doctorstrange.jpg')
+INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (3, 2, N'Doctor Strange', 13.00, N'images/products/doctorstrange.jpg')
 GO
-INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (5, 2, N'Fanatastic Beasts and Where To Find Them', 12.5000, N'images/products/fantasticbeasts.jpg')
+INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (5, 2, N'Fanatastic Beasts and Where To Find Them', 12.50, N'images/products/fantasticbeasts.jpg')
 GO
-INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (7, 1, N'The Girl On The Train', 10.0000, N'images/products/girlonthetrain.jpg')
+INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (7, 1, N'The Girl On The Train', 10.00, N'images/products/girlonthetrain.jpg')
 GO
-INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (9, 1, N'John Wick', 5.0000, N'images/products/johnwick.jpg')
+INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (9, 1, N'John Wick', 5.00, N'images/products/johnwick.jpg')
 GO
-INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (11, 1, N'Deepwater Horizon', 9.8800, N'images/products/deepwaterhorizon.jpg')
+INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (11, 1, N'Deepwater Horizon', 9.88, N'images/products/deepwaterhorizon.jpg')
 GO
-INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (14, 1, N'Deadpool', 6.7000, N'images/products/deadpool.jpg')
+INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (14, 1, N'Deadpool', 6.70, N'images/products/deadpool.jpg')
 GO
-INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (15, 2, N'Arrival', 12.0000, N'images/products/arrival.jpg')
+INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (15, 2, N'Arrival', 12.00, N'images/products/arrival.jpg')
 GO
-INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (16, 1, N'Beauty and The Beast', 9.9900, N'images/products/beautyandthebeast.jpg')
+INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (16, 1, N'Beauty and The Beast', 9.99, N'images/products/beautyandthebeast.jpg')
 GO
-INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (17, 1, N'Hidden Figures', 8.5000, N'images/products/hiddenfigures.jpg')
+INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (17, 1, N'Hidden Figures', 8.50, N'images/products/hiddenfigures.jpg')
 GO
-INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (18, 2, N'Logan', 10.2900, N'images/products/logan.jpg')
+INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (18, 2, N'Logan', 10.29, N'images/products/logan.jpg')
 GO
-INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (20, 2, N'The Lego Batman Movie', 14.9600, N'images/products/legobatman.jpg')
+INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (20, 2, N'The Lego Batman Movie', 14.96, N'images/products/legobatman.jpg')
 GO
-INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (21, 1, N'The Magnificent Seven', 7.9900, N'images/products/magnificentseven.jpg')
+INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (21, 1, N'The Magnificent Seven', 7.99, N'images/products/magnificentseven.jpg')
 GO
-INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (22, 2, N'Star Trek Beyond', 13.0000, N'images/products/startrekbeyond.jpg')
+INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (22, 2, N'Star Trek Beyond', 13.00, N'images/products/startrekbeyond.jpg')
 GO
-INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (23, 1, N'Suicide Squad', 1.2000, N'images/products/suicidesquad.jpg')
+INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (23, 1, N'Suicide Squad', 1.20, N'images/products/suicidesquad.jpg')
 GO
-INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (24, 1, N'Sully: Miracle On The Hudson', 9.9900, N'images/products/sully.jpg')
+INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (24, 1, N'Sully: Miracle On The Hudson', 9.99, N'images/products/sully.jpg')
 GO
-INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (25, 2, N'The Revenant', 7.5000, N'images/products/therevenant.jpg')
+INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (25, 2, N'The Revenant', 7.50, N'images/products/therevenant.jpg')
 GO
-INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (26, 2, N'X-Men Apocalypse', 5.0000, N'images/products/xmenapocalypse.jpg')
+INSERT [dbo].[Products] ([Id], [CategoryId], [Name], [Price], [ImageUrl]) VALUES (26, 2, N'X-Men Apocalypse', 5.00, N'images/products/xmenapocalypse.jpg')
 GO
 SET IDENTITY_INSERT [dbo].[Products] OFF
 GO
